13f7478cd64b54601842223c5e8f42f11ff6cbb2
ryantoddgarza <ryantoddgarza@gmail.com>
2020-12-04 22:07:39 -0600

    Try another nvm lazy load approach

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -1,21 +1,14 @@
 # Vim mode
 bindkey -v
 
-# lazy load nvm FIXME
-# NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 4 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
-# NODE_GLOBALS+=(node nvm)
+# Lazy load nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use
 
-# load_nvm () {
-#   [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
-# }
-
-# for cmd in "${NODE_GLOBALS[@]}"; do
-#   eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@; }"
-# done
-
-# always load nvm
-[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
-
+function node npm yarn {
+  unfunction node npm yarn
+  nvm use default
+  command "$0" "$@"
+}
 
 # Completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'l:|=* r:|=*'
