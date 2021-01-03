cd1383f79aa0e5e7d08aa170c6840307cf5949ce
ryantoddgarza <ryantoddgarza@gmail.com>
2021-01-03 15:13:03 -0600

    Add url aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -30,5 +30,10 @@ alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 # folders
 alias ongoing="cd ~/Code/Ongoing"
 
+# urls
+alias chrome="open -a 'Google Chrome'"
+alias gcal="chrome https://calendar.google.com/"
+alias gmail="chrome https://mail.google.com/"
+
 # misc
 alias bsserve="browser-sync start -sf ."
