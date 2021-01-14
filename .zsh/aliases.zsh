ce66d506652f4dc0f7577c155a726483b6eca14b
ryantoddgarza <ryantoddgarza@gmail.com>
2021-01-14 16:58:08 -0600

    Update aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,9 @@
+# bin
+alias ls="ls -F"
+alias cp="cp -iv"
+alias mv="mv -iv"
+alias du="du -csh"
+
 # system
 alias shutdown="sudo shutdown -h now"
 alias restart="sudo shutdown -r now"
@@ -8,16 +14,10 @@ alias vconfig="vim ~/.vimrc"
 alias tconfig="vim ~/.tmux.conf"
 alias gconfig="vim ~/.gitconfig"
 
-# bin
-alias ls="ls -F"
-alias cp="cp -iv"
-alias mv="mv -iv"
-alias du="du -csh"
-
 # tmux
 alias txls="tmux list-sessions"
 alias txns="tmux new-session -s"
-alias txkt="tmux kill-session -t"
+alias txks="tmux kill-session -t"
 alias txat="tmux attach -t"
 
 # vim
@@ -25,7 +25,7 @@ alias v="vim ."
 
 # git
 alias g="git"
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias dotf="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 # folders
 alias ongoing="cd ~/Code/Ongoing"
