e607cc9806ac4be909e47c37bf69f7bdc04dda13
ryantoddgarza <ryantoddgarza@gmail.com>
2021-01-22 22:47:29 -0600

    Move script utility commands out of main function

--- .macos/macos-defaults.sh
+++ .macos/macos-defaults.sh
@@ -2,14 +2,13 @@
 
 set -e
 
+# Actions made relative to this file's path
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Load utils
+source utils.sh
+
 main() {
-  # Ensure the following actions are
-  # made relative to this file's path
-  cd "$(dirname "${BASH_SOURCE[0]}")"
-
-  # Load utils
-  source utils.sh
-
   # Immediately validate sudo user
   validate_sudo
 
