61df282f505d0aa37086e7b5fe2eb402e520efbf
ryantoddgarza <ryantoddgarza@gmail.com>
2021-02-03 13:49:23 -0600

    fix: old style command substitution breaks rename

--- .macos/preferences/screencapture.sh
+++ .macos/preferences/screencapture.sh
@@ -16,7 +16,7 @@ defaults write com.apple.screencapture include-date -bool false
 defaults write com.apple.screencapture type -string "jpg"
 
 # Prefix screenshots filename
-defaults write com.apple.screencapture name -string "`hostname`-`date +"%Y%j_%H%M%S"`"
+defaults write com.apple.screencapture name -string "$(hostname)-$(date +"%Y%j_%H%M%S")"
 
 # Disable drop shadow
 defaults write com.apple.screencapture disable-shadow -bool true
