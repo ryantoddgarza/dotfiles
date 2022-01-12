988d9ae20eeddd086af2bf5e10fa90a2c5a1945b
ryantoddgarza <ryantoddgarza@gmail.com>
2022-01-12 13:10:42 -0600

    feat: enable tap to click for built-in trackpad

--- .macos/preferences/input_devices.sh
+++ .macos/preferences/input_devices.sh
@@ -6,3 +6,8 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # Set a fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 20
+
+# Trackpad: enable tap to click
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 0
