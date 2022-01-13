d57da924d892edc701b985d807724c3ac12989a3
ryantoddgarza <ryantoddgarza@gmail.com>
2022-01-13 14:09:09 -0600

    feat: remap caps lock to escape with launch agent

--- .macos/preferences/input_devices.sh
+++ .macos/preferences/input_devices.sh
@@ -11,3 +11,24 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 20
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 0
+
+# Remap caps lock to escape
+cat > $HOME/Library/LaunchAgents/com.ryantoddgarza.CapslockEsc.plist <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>com.ryantoddgarza.CapslockEsc</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>/usr/bin/hidutil</string>
+      <string>property</string>
+      <string>--set</string>
+      <string>{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+  </dict>
+</plist>
+EOF
