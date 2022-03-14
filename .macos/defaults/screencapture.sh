218c19589a1730df48d15255c5970f5550f07393
ryantoddgarza <ryantoddgarza@gmail.com>
2022-03-13 23:49:14 -0500

    refactor: screencapture datestring

--- .macos/defaults/screencapture.sh
+++ .macos/defaults/screencapture.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Screencapture directory
+# Set screencapture directory
 screencapture_dir="$HOME/Pictures/Screencapture"
-
-# Create screencapture directory if it doesn't exist
 mkdir -p $screencapture_dir
 
-# Save screenshots to specified directory
+# Save screencapture to specified directory
 defaults write com.apple.screencapture location -string "$screencapture_dir"
+
+# Save screencapture as jpg
+defaults write com.apple.screencapture type -string "jpg"
 
 # Remove default timestamp
 defaults write com.apple.screencapture include-date -bool false
 
-# Save screenshots as jpg
-defaults write com.apple.screencapture type -string "jpg"
-
-# Prefix screenshots filename
-defaults write com.apple.screencapture name -string "$(hostname)-$(date +"%Y%j_%H%M%S")"
+# Set screencapture filename
+defaults write com.apple.screencapture name -string "$(hostname)-$(date +"%Y%m%dT%H%M%S")"
 
 # Disable drop shadow
 defaults write com.apple.screencapture disable-shadow -bool true
