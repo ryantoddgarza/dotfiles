3f5e20c422464e33d611f695d19d6b1504c67cf0
ryantoddgarza <ryantoddgarza@gmail.com>
2022-03-15 11:08:59 -0500

    refactor: remove disable airdrop

--- .macos/defaults/network.sh
+++ .macos/defaults/network.sh
@@ -6,6 +6,3 @@ sudo networksetup -setdnsservers Wi-Fi \
   1.0.0.1 \
   2606:4700:4700::1111 \
   2606:4700:4700::1001
-
-# Disable AirDrop
-defaults write com.apple.NetworkBrowser DisableAirDrop -bool true
