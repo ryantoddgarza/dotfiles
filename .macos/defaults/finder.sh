ae270772143726fba73066fca366044b43fba292
ryantoddgarza <ryantoddgarza@gmail.com>
2022-03-15 11:49:15 -0500

    feat: hide recent tags in Finder

--- .macos/defaults/finder.sh
+++ .macos/defaults/finder.sh
@@ -7,5 +7,8 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 # Open new Finder window in home directory
 defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME"
 
+# Hide recent tags
+defaults write com.apple.finder ShowRecentTags -bool false
+
 # Kill all Finder processes
 killall Finder
