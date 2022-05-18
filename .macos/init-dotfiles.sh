3135c4a07a9b8b94f038c7f51564d2fddc3f2276
ryantoddgarza <ryantoddgarza@gmail.com>
2022-05-17 20:43:43 -0500

    feat: checkout before updating submodules

--- .macos/init-dotfiles.sh
+++ .macos/init-dotfiles.sh
@@ -5,6 +5,7 @@ set -e
 main() {
   dotfiles="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
+  $dotfiles checkout --force
   $dotfiles submodule update --init --recursive
   $dotfiles config status.showUntrackedFiles no
 
