f28fb099e6ac0189044eeae3227a5e21aac73c35
ryantoddgarza <ryantoddgarza@gmail.com>
2022-05-17 20:36:24 -0500

    refactor: add recursive flag to submodule update command

--- .macos/init-dotfiles.sh
+++ .macos/init-dotfiles.sh
@@ -5,7 +5,7 @@ set -e
 main() {
   dotfiles="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
-  $dotfiles submodule update --init
+  $dotfiles submodule update --init --recursive
   $dotfiles config status.showUntrackedFiles no
 
   unset dotfiles
